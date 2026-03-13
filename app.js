33410d887fad0c5e395e0ed7eddadede05807ea7
yessid <tucorreo@gmail.com>
2026-03-13 12:16:55 -0500

    fix express public path

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.set("io", io);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static("public"))
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public",
     "index.html"));
