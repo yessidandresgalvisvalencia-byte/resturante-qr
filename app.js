51dcab4888057e455c4085f7ebb2c321b5cccf3f
yessid <tucorreo@gmail.com>
2026-03-18 11:31:59 -0500

    fix static path

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set("io", io);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use("/api", apiRoutes);
 
