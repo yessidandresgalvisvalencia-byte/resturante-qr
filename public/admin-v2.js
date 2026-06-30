9d86535f458395e5bca33e917e16820c6821c130
yessid <tucorreo@gmail.com>
2026-06-30 09:14:53 -0500

    continuar

--- public/admin-v2.js
+++ public/admin-v2.js
@@ -18,6 +18,13 @@ async function cargarModuloGRUK(modulo) {
   const html = await res.text();
 
   contenedor.innerHTML = html;
+  if (modulo === "laboral") {
+  const linkLaboral = document.getElementById("linkModuloLaboralGRUK");
+
+  if (linkLaboral) {
+    linkLaboral.href = `/laboral.html?restaurantId=${adminRestaurantId}`;
+  }
+}
 
   const menu = document.getElementById("menuAdminGRUK");
   if (menu) menu.classList.remove("abierto");
